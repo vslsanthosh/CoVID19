6370f732b9933dfc54fe94021cea4eaf3edcfe98
vslsanthosh <vslsanthosh@users.noreply.github.com>
2020-04-28 20:36:40 +0530

    Refractor

--- CoVID19/Program.cs
+++ CoVID19/Program.cs
@@ -7,10 +7,7 @@ namespace CoVID19
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
-
             var json = GetJSONData("https://api.covid19india.org/raw_data.json");
-
             Console.WriteLine(json.Content.ReadAsStringAsync().Result);
         }
 
