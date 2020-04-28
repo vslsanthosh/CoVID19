f004ff5044dafa13eb9ac00463f8da0b2f4baa48
vslsanthosh <vslsanthosh@users.noreply.github.com>
2020-04-28 20:33:40 +0530

    Add project files.

---
+++ CoVID19/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Net.Http;
+
+namespace CoVID19
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            var json = GetJSONData("https://api.covid19india.org/raw_data.json");
+
+            Console.WriteLine(json.Content.ReadAsStringAsync().Result);
+        }
+
+        static HttpResponseMessage GetJSONData(string uri)
+        {
+            using var client = new HttpClient();
+            var result = client.GetAsync(uri);
+            return result.Result;
+        }
+    }
+}
